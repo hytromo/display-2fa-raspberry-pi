d2ccc71f87c2213484f01456ad6b39903456f7f0
Alexandros Solanos <alex@uizard.io>
2023-02-09 21:50:34 +0200

    First

---
+++ README.md
@@ -0,0 +1,25 @@
+# Disclaimer
+
+1. If you leak your 2FA secrets it's like you don't have 2FA turned on. It's a big security risk. Make sure you understand what having your 2FA secrets available inside your RPi means.
+2. This is just a pet project written within a few hours. Do not expect to find optimized code or best practices. Feel free to make PRs to improve it.
+3. The code as is works only for 2FA codes that rotate every 30 seconds, at :00 and :30 of every minute.
+
+# Hardware Setup
+
+1. [Waveshare display](https://www.waveshare.com/1.44inch-lcd-hat.htm)
+2. [RPi 4b](https://www.raspberrypi.com/products/raspberry-pi-4-model-b/) - but it should work with any RPi compatible with the screen
+
+# Software Setup
+
+0. Clone this repo inside your RPi.
+1. Install all the required drivers for the display. Run some provided example to make sure that the screen works fine.
+2. Install python 3 if it's missing from your system.
+3. `pip install -r requirements.txt`.
+4. Find your 2FA secrets. That's the strings required to generate the original QR codes you scan and the 2FA codes that you are presented with. I used [this gist](https://gist.github.com/gboudreau/94bb0c11a6209c82418d01a59d958c93) to extract them from Authy, which works at the time of writing this. Alternatively, if you reset your 2FA, most websites give you this secret string along with the QR code.
+5. Replace the logos and the code to contain the services you want. You may alter any global consts as you see fit.
+6. Export your secrets based on the environment variable names in the code, e.g. `export GOOGLE_2FA_SECRET=abcdef..`
+7. Run the script `python main.py` and voila.
+
+# Why
+
+2FA codes in phone can be distracting and it looks cool.
---
+++ main.py
@@ -0,0 +1,160 @@
+import LCD_1in44
+import LCD_Config
+from datetime import datetime
+from os import path, environ
+import subprocess
+
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageEnhance
+
+# Images will become thumbnails with the same aspect ratio as the original
+IMAGE_WIDTH = 28
+IMAGE_HEIGHT = 24
+CENTER_IMAGE_IN_FIRST_N_PIXELS = 35
+
+FONT_PATH = "/usr/share/fonts/truetype/noto/NotoSansMono-Regular.ttf"
+FONT_SIZE = 22
+
+PROGRESSBAR_COLOR = "WHITE"
+PROGRESSBAR_WIDTH = 2
+
+# Blur effect
+CODE_ROTATION_EFFECT_ENABLED = True
+
+# 0, 90, 180, 270 depending on how your RPi stands
+DISPLAY_ROTATION_DEGREES = 90
+
+LCD = LCD_1in44.LCD()
+LCD.LCD_Init(LCD_1in44.SCAN_DIR_DFT)
+
+
+canvas_with_images = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+
+
+def include_in_canvas_and_return(rel_path, index):
+    global canvas_with_images
+    final_path = path.join(path.dirname(path.abspath(__file__)), rel_path)
+    img = Image.open(final_path)
+    img.thumbnail(
+        [IMAGE_WIDTH, IMAGE_HEIGHT], Image.AFFINE)
+    canvas_with_images.paste(
+        img, (int((CENTER_IMAGE_IN_FIRST_N_PIXELS - img.width) / 2), int(index * LCD.height / 4 + (LCD.height / 4 - img.height) / 2)))
+    return img
+
+
+display_definitions = {
+    'AWS': {
+        'image': include_in_canvas_and_return('logos/aws.png', 0),
+        'code': environ.get('AWS_2FA_SECRET'),
+        'color': '#ed9f2a'
+    },
+    'Google': {
+        'image': include_in_canvas_and_return('logos/google.webp', 1),
+        'code': environ.get('GOOGLE_2FA_SECRET'),
+        'color': '#c85138'
+    },
+    'GitHub': {
+        'image': include_in_canvas_and_return('logos/github.png', 2),
+        'code': environ.get('GITHUB_2FA_SECRET'),
+        'color': '#ffffff'
+    },
+    'Bitwarden': {
+        'image': include_in_canvas_and_return('logos/bitwarden.png', 3),
+        'code': environ.get('BITWARDEN_2FA_SECRET'),
+        'color': '#345dd9'
+    }
+}
+
+
+font = ImageFont.truetype(FONT_PATH, size=FONT_SIZE)
+font_offset = font.getoffset('123456')
+
+
+def display_message_centered_at_index(draw, message, index, color):
+    _, _, w, h = draw.textbbox((0, 0), message, font=font)
+    w += font_offset[0]
+    h += font_offset[1]
+    draw.text(((LCD.width-w+CENTER_IMAGE_IN_FIRST_N_PIXELS)/2, (LCD.height/4-h)/2 +
+              index*(LCD.height/4)), message, font=font, fill=color)
+
+
+def output_of(cmd):
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+    return result.stdout.decode("utf-8").strip()
+
+
+def display_key(draw, code, index, color):
+    twofa = output_of(['oathtool', '-b', '--totp', code])
+    display_message_centered_at_index(draw, twofa, index, color)
+
+
+def refresh_image(canvas):
+    if DISPLAY_ROTATION_DEGREES == 0:
+        LCD.LCD_ShowImage(canvas, 0, 0)
+    else:
+        LCD.LCD_ShowImage(canvas.rotate(DISPLAY_ROTATION_DEGREES), 0, 0)
+
+
+def pick_between_gradient(color1, color2, weight):
+    w2 = weight
+    w1 = 1.0 - w2
+    (c1r, c1g, c1b) = tuple(int(color1[i:i+2], 16) for i in (0, 2, 4))
+    (c2r, c2g, c2b) = tuple(int(color2[i:i+2], 16) for i in (0, 2, 4))
+    (resultR, resultG, resultB) = (
+        int(float(c1r)*w1+float(c2r)*w2),
+        int(float(c1g)*w1+float(c2g)*w2),
+        int(float(c1b)*w1+float(c2b)*w2),
+    )
+    return '#%02x%02x%02x' % (resultR, resultG, resultB)
+
+
+def main():
+    def update_codes_loop(draw, canvas):
+        for index, key in enumerate(display_definitions):
+            display_key(
+                draw, display_definitions[key]['code'], index, display_definitions[key]['color'])
+
+        refresh_image(canvas)
+
+    while True:
+        canvas = canvas_with_images.copy()
+        draw = ImageDraw.Draw(canvas)
+
+        update_codes_loop(draw, canvas)
+
+        # progress bar
+        while True:
+            now = datetime.now()
+            second_raw = now.second + now.microsecond / 1e6
+            second = second_raw if second_raw <= 30 else second_raw - 30
+            loaded_fraction = second / 30
+            percentage = min(loaded_fraction, 1)
+            draw.line([(LCD.width-1, LCD.height), (LCD.width-1, (1-percentage) *
+                                                   LCD.height)], fill=PROGRESSBAR_COLOR, width=2)
+
+            refresh_image(canvas)
+
+            if loaded_fraction > 0.995:
+                # let's draw it black now because this white line messes a bit with the blur
+                draw.line([(LCD.width-1, LCD.height), (LCD.width-1,
+                                                       0)], fill="black", width=PROGRESSBAR_WIDTH)
+                refresh_image(canvas)
+
+                if CODE_ROTATION_EFFECT_ENABLED:
+                    canvas_orig = canvas
+                    for i in range(10):
+                        enhance = ImageEnhance.Brightness(canvas_orig)
+                        canvas = enhance.enhance(1-0.12*i)
+                        canvas = canvas.filter(
+                            ImageFilter.GaussianBlur(radius=1 + i*0.3))
+                        canvas.putalpha(255 - i * 25)
+                        refresh_image(canvas)
+                        LCD_Config.Driver_Delay_ms(50)
+                else:
+                    # make sure that we have the new codes ready
+                    LCD_Config.Driver_Delay_ms(200)
+
+                break  # update codes
+
+
+if __name__ == '__main__':
+    main()
